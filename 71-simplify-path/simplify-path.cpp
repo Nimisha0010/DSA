0d6f7bba36c91ce99a522f2319654ddeba1ac2a6
Nyx0010 <nimishat551@gmail.com>
2026-07-11 22:11:16 +0530

    Time: 1 ms (76.15%) | Memory: 11.6 MB (44.28%) - LeetSync

---
+++ 71-simplify-path/simplify-path.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    string simplifyPath(string path) {
+        stack<string> st;
+        string curr;
+        for(int i=0;i<= path.size();i++){
+            if (i == path.size() || path[i] == '/') {
+                if(curr == "" || curr == "."){
+                }
+                else if(curr ==".."){
+                    if(!st.empty())
+                    st.pop();
+                }
+                else{
+                    st.push(curr);
+                }
+                curr.clear();
+            }
+            else{
+                curr += path[i];
+            }
+        }
+        if(st.empty())
+        return "/";
+
+        vector<string> folders;
+        while(!st.empty()){
+            folders.push_back(st.top());
+            st.pop();
+        }
+        reverse(folders.begin() , folders.end());
+        string ans;
+        for(string folder : folders){
+            ans += "/" + folder;
+        }
+        return ans;
+    }
+};
