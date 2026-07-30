341b471756b1b0a4aea61aa7f43dd8ee98fb83d9
Nyx0010 <nimishat551@gmail.com>
2026-07-30 15:15:28 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (54.67%) - LeetSync

---
+++ 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int minimumPushes(string word) {
+        int ans =0 , n = word.length();
+        for(int i =0; i<n;i++){
+            ans +=(i/8)+1;
+        }
+        return ans;
+    }
+};
